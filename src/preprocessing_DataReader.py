12674eb0fb8b4449114da17cce04fb5c3986f844
FWalkowicz <fillio00@wp.pl>
2025-05-15 17:14:31 +0200

    reading single edf

--- src/preprocessing_DataReader.py
+++ src/preprocessing_DataReader.py
@@ -1,3 +1,5 @@
+from tqdm import tqdm
+from utils.channel_names import CHANNEL_NAMES
 import numpy as np
 import pandas as pd
 import mne
@@ -7,21 +9,37 @@ import os
 class PreprocessingDataReader:
     def __init__(self, path: str, patient: Union[int|List[int]]) -> None:
         self.path = path
-        self.patent = patient if isinstance(patient, list) else [patient]
+        self.patient = patient if isinstance(patient, list) else [patient]
         self.data = []
 
     def _get_path(self, patient: int) -> str:
-        try:
-            os.path.join(self.path, )
-        except FileNotFoundError:
-            pass
+        return os.path.join(self.path, f"S{patient:03d}")
 
-    def _read_edf_file(self):
-        pass
+
+    def _read_edf_file(self, file_path: int) -> pd.DataFrame:
+        patient_path = self._get_path(file_path)
+        if not os.path.exists(patient_path):
+            print(f"Patient folder not found: {patient_path}")
+
+        try:
+            raw = mne.io.read_raw_edf(file_path, preload=True)
+            annotations = raw.annotations
+            edf_data = pd.DataFrame(
+                raw.get_data(),
+                columns=CHANNEL_NAMES,
+            )
+            return edf_data
+        except Exception as e:
+            print(f"Could not read {file_path}: {e}")
+
 
     def load(self):
+        pass
+
+    def normalize(self):
         pass
 
 if __name__ == '__main__':
     path = '../data/physionet.org/files/eegmmidb/1.0.0'
     data = PreprocessingDataReader(path=path, patient=[1, 2])
+    patients_data = data._read_edf_file(1)
