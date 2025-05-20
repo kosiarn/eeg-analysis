f6d4d9ac3b3907e6de1f39c8c1549ebeebdfb8f1
kosiarn <krystiannnurban@gmail.com>
2025-05-20 04:29:55 +0200

    feat(wavelet transformed data FC net): training loop

--- src/fc_nets.py
+++ src/fc_nets.py
@@ -1,7 +1,11 @@
+from math import floor
 import torch
 from utils.channel_names import CHANNEL_NAMES
-from utils.data_specs import NUM_STATES
+from utils.data_specs import NUM_STATES, NUM_PATIENTS
+from utils.training import get_run_title, one_hot_encode_target
 from dataloader import EEGDataLoader
+from random import shuffle
+from tqdm import tqdm
 
 class WaveletFCNet(torch.nn.Module):
     """
@@ -23,8 +27,27 @@ class WaveletFCNet(torch.nn.Module):
 if __name__ == "__main__":
     wavelet_net = WaveletFCNet(len(CHANNEL_NAMES), 1)
     data = EEGDataLoader('../data/physionet.org/files/eegmmidb/1.0.0')
-    data.load(1, 1)
-    test_sample = data.__getitem__(1)[0]
-    output = wavelet_net(test_sample)
-    print(output)
     
+    optimizer = torch.optim.SGD(wavelet_net.parameters(), lr = 0.01, momentum = 0.9)
+    criterion = torch.nn.CrossEntropyLoss()
+
+    run_title = get_run_title("wavelet_fc")
+    experiment_index = 5
+    num_epochs = 1000
+    patients = [patient + 1 for patient in range(NUM_PATIENTS - 1)]
+    shuffle(patients)
+    training_set = patients[:floor(0.7 * NUM_PATIENTS)]
+    data.load(training_set, experiment_index)
+    for epoch in range(num_epochs):
+        wavelet_net.train()
+        running_loss = 0.0
+        print(f"Epoch {epoch}...")
+        for (sample, target) in tqdm(data):
+            ground_truth = one_hot_encode_target(target)
+            predicted = wavelet_net(sample)
+            loss = criterion(predicted, ground_truth)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        if epoch % 10 == 0:
+            torch.save(wavelet_net.state_dict(), f"./models/{run_title}/checkpoint_e{epoch}")
--- src/utils/data_specs.py
+++ src/utils/data_specs.py
@@ -3,3 +3,4 @@ SamplesPerSecond = int
 sample_rate: SamplesPerSecond = 160
 
 NUM_STATES = 3
+NUM_PATIENTS = 109
